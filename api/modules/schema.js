29dfba019f3a4ecfb7f007d762c7fd5dc4c68566
yash-251198 <shetyeyash444@gmail.com>
2024-12-31 01:27:37 +0530

    Updated the schema model and edited the get response structure

--- api/modules/schema.js
+++ api/modules/schema.js
@@ -1,10 +1,10 @@
 import mongoose from 'mongoose';
 
-const productSchema =mongoose.Schema({
+const productSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
-    name:String,
-    price:Number
+    name: { type: String, required: true },
+    price: { type: Number, required: true }
 })
 
-const product = mongoose.model('Product',productSchema);
+const product = mongoose.model('Product', productSchema);
 export default product;
--- api/routes/product.js
+++ api/routes/product.js
@@ -1,67 +1,82 @@
-import express from 'express'; 
+import express from 'express';
 export const routerProduct = express.Router();
 import Product from '../modules/schema.js';
 import mongoose from 'mongoose';
 
 
-routerProduct.get('/',(req,res,next)=>{
-   Product.find().then((docs) => {
-    res.status(200).json(docs);
-   }).catch((err) => {
-    res.status(500).json(err);
-   });
-})
+routerProduct.get('/', (req, res, next) => {
 
-routerProduct.post('/',(req,res,next) =>{
-    const product = new Product({
-        _id:new mongoose.Types.ObjectId,
-        name:req.body.name,
-        price:req.body.price
-    });
-    product.save().then((result) => {
+    Product.find().select("name price _id").then((docs) => {
+        const response = {
+            count: docs.length,
+
+            products: docs.map(doc => {
+                return {
+                    name: doc.name,
+                    price: doc.price,
+                    _id: doc._id,
+                    request: {
+                        type: 'GET',
+                        url: 'http://localhost:3000/products/' + doc.id
+                    }
+                }
+            })
+        }
+
+        res.status(200).json(response);
     }).catch((err) => {
-        console.log(err)
+        res.status(500).json(err);
     });
-    res.status(201).json({
-        message:"product created",
-        product : product
-    })
 })
 
-routerProduct.get('/:productId',(req,res,next) =>{
+routerProduct.post('/', (req, res, next) => {
+    const product = new Product({
+        _id: new mongoose.Types.ObjectId,
+        name: req.body.name,
+        price: req.body.price
+    });
+    product.save().then((doc) => {
+        res.status(201).json(doc)
+    }).catch((err) => {
+        res.status(500).json({Error:err})
+    });
+})
+
+routerProduct.get('/:productId', (req, res, next) => {
     const id = req.params.productId;
+
     Product.findById(id).exec().then((doc) => {
-        if(doc){
+        if (doc) {
             res.status(200).json(doc)
-        }else{
-            res.status(404).json({message:"There is no entry with this id"});
+        } else {
+            res.status(404).json({ message: "There is no entry with this id" });
         }
     }).catch((err) => {
-        res.status(500).json({Error:err});
+        res.status(500).json({ Error: err });
     });
 })
 
 
-routerProduct.patch('/:id',(req,res,next) =>{
+routerProduct.patch('/:id', (req, res, next) => {
     const id = req.params.id;
     const updateOps = {};
-    for(const ops of req.body){
+    for (const ops of req.body) {
         updateOps[ops.propName] = ops.value;
     }
 
-    Product.findOneAndUpdate({_id : id},{$set:updateOps},{new : true}).exec().then(doc =>{
+    Product.findOneAndUpdate({ _id: id }, { $set: updateOps }, { new: true }).exec().then(doc => {
         res.status(200).json(doc);
     }).catch(err => {
-        res.status(500).json({Error:err}) 
+        res.status(500).json({ Error: err })
     })
 })
 
-routerProduct.delete('/:id',(req,res,next) =>{
+routerProduct.delete('/:id', (req, res, next) => {
     const id = req.params.id;
     Product.findByIdAndDelete(id).exec().then((result) => {
         res.status(200).json(result)
     }).catch((err) => {
-        res.status(500).json({Error:err})
+        res.status(500).json({ Error: err })
     });
 })
 
